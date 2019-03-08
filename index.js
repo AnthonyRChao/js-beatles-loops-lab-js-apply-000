d78e0ac35dc549b3d87f33a1cebc46b3feec7409
Anthony Chao <anthony.chao@blackrock.com>
2019-03-08 23:58:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+  let res = [];
+  for (var i = 0; i < musicians.length; i++) {
+    let str = musicians[i] + " plays " + instruments[i];
+    res.push(str);
+  }
+  return res;
+}
+
+function johnLennonFacts (facts) {
+  let res = [];
+  while (facts.length >= 0) {
+    let str = facts.pop() + "!!!";
+    res.push(str);
+  }
+  return res;
+}
